5baee0668512d773fca3e76350a78b4780d2f99c
Arda Mavi <ardamavi2@gmail.com>
2018-03-01 20:30:50 +0300

    User interface updated

--- live.py
+++ live.py
@@ -19,20 +19,31 @@ def main():
     model = model_from_json(model)
     # Getting weights
     model.load_weights("Data/Model/weights.h5")
+    
+    print('Press "ESC" button for exit.')
 
     # Get image from camera, get predict and say it with another process, repeat.
     cap = cv2.VideoCapture(0)
     old_char = ''
     while 1:
         ret, img = cap.read()
-        cv2.imshow('Arda Mavi',img)
+        
+        # Cropping image:
+        img_height, img_width = img.shape[:2]
+        side_width = int((img_width-img_height)/2)
+        img = img[0:img_height, side_width:side_width+img_height]
+        
+        # Show window:
+        cv2.imshow('VSL',cv2.flip(img,1)) # Flip(mirror effect) for easy handling.
+        
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = imresize(img, (img_size, img_size, channel_size))
         img = 1-np.array(img).astype('float32')/255.
         img = img.reshape(1, img_size, img_size, channel_size)
+        
         Y_string, Y_possibility = predict(model, img)
-        print(Y_string, Y_possibility)
         if(platform.system() == 'Darwin') and old_char != Y_string and Y_possibility > 0.6:
+            print(Y_string, Y_possibility)
             arg = 'say {0}'.format(Y_string)
             # Say predict with multiprocessing
             Process(target=os.system, args=(arg,)).start()
